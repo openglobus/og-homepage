332f535a6cc003cec636f98e4c83a510217f30b3
Zemledelec <mgevlich@gmail.com>
2018-05-05 18:51:23 +0300

    og on webpack

--- start.js
+++ start.js
@@ -1,35 +1,23 @@
 ﻿function start() {
-    //og.RESOURCES_URL = "./resources/";
 
     var osm = new og.layer.XYZ("OpenStreetMap", {
-        specular: [0.0003, 0.00012, 0.00001],
-        //extent: [[0, 0], [45, 45]],
-        shininess: 20,
-        diffuse: [0.89, 0.9, 0.83],
         isBaseLayer: true,
         url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         visibility: true,
         attribution: 'Data @ <a href="http://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="http://www.openstreetmap.org/copyright">ODbL</a>'
     });
 
-    var controls = [
-        og.control.mouseNavigation(),
-	    og.control.touchNavigation(),
-    	og.control.zoomControl(),
-        og.control.earthCoordinates(),
-    	og.control.sun()
-    ];
-
-    globus = new og.Globus({
+    var globus = new og.Globe({
         "target": "globus",
         "name": "Earth",
-        "controls": controls,
-        "terrain": new og.terrainProvider.TerrainProvider("OpenGlobus"),
+        "terrain": new og.terrain.GlobusTerrain(),
         "layers": [osm],
         "autoActivated": true
     });
 
-    og.layer.vector("Markers", { groundAlign: true })
+    new og.layer.Vector("Markers", {
+        clampToGround: true
+    })
         .addTo(globus.planet)
         .add(new og.Entity({
             lonlat: [5.73, 45.183],
@@ -52,5 +40,4 @@
 
 	
     globus.planet.viewExtentArr([5.54,45.141,5.93,45.23]);
-
 };
