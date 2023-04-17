f030f51e5e24e72c45c33c4e6cca8cb80431a675
Zemledelec <mgevlich@gmail.com>
2023-04-17 17:34:56 +0400

    cleanup

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import "./fontello/css/fontello.css";
+
+const App = () => {
+    return (
+        <div className="App">
+
+            <div className="og-videobackground">
+                <video id="background-video" autoplay loop muted playsinline>
+                    <source src="./video.mp4" type="video/mp4"/>
+                </video>
+            </div>
+
+            <div className="og-gradient"></div>
+
+            <div className="og-underconstruction">UNDER CONSTRUCTION</div>
+
+            <div className="og-main">
+                <div className="og-top">
+                    <div className="og-brand">Openglobus</div>
+                    <div className="og-social">
+                        <a className="nav-link" href="https://github.com/openglobus/openglobus" title="GitHub"><i
+                            className="icon-github-circled"
+                            aria-hidden="true"></i></a>
+                        <a className="nav-link" href="https://twitter.com/openglobus" title="Twitter"><i
+                            className="icon-twitter"
+                            aria-hidden="true"></i></a>
+                        <a className="nav-link" href="https://www.linkedin.com/company/openglobus/" title="LinkedIn"><i
+                            className="icon-linkedin"
+                            aria-hidden="true"></i></a>
+                        <a className="nav-link" href="https://groups.google.com/g/openglobus" title="Google Forum"><i
+                            className="icon-google"
+                            aria-hidden="true"></i></a>
+                    </div>
+                </div>
+
+                <div className="og-center">
+                    <div className="og-description">
+                        &emsp;Openglobus is an open-source javascript library for creating interactive 3D maps,
+                        with real-time
+                        surroundings rendering. It supports various high-resolution terrain providers, and imagery
+                        layers,
+                        renders thousands of 3D objects, provides geometry measurement tools, and more.
+                    </div>
+                </div>
+
+                <div className="og-subscribe">
+                    Stay tuned, and subscribe!
+                </div>
+            </div>
+
+            <div className="og-bottom">
+                <p className="og-copyright">© openglobus 2023. <a
+                    href="https://github.com/openglobus/openglobus/blob/master/LICENSE.md">License MIT</a></p>
+            </div>
+
+        </div>
+    )
+        ;
+}
+
+export default App;
