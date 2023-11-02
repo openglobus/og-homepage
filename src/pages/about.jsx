db76117a0315f10d8dc16c0f58023d924877e905
Zemledelec <mgevlich@gmail.com>
2023-11-02 20:15:36 +0400

    Added about description

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,12 +2,24 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
-            </h1>
-        </div>
+        <>
+            <div className="og-center">
+                <img className="og-about-background" src="./atmos2.png"></img>
+                <div className="og-about">
+                    &emsp;&emsp;Openglobus was created in 2013 year by software engineer Michael Gevlich. It was based
+                    on the idea of
+                    developing very simple graphics engine based on pure WebGL API, without any additional libraries or
+                    frameworks. Later, one of the main elements of the engine became a scene for rendering the planet
+                    Earth with
+                    the relief of SRTM terrain data.
+                    The accuracy of the data of the planet allows visualizing geospatial data at the scale of the
+                    whole planet and ant at the same time, and the planet engine allows you to work with the highest
+                    precision terrain data.
+                    The main ideas for working with the planet engine are simplicity, rigor, beauty and visual quality
+                    of the picture, and ease of navigation.
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,38 +40,47 @@ const Home = () => {
             <div className="og-gradient"></div>
 
             <div className="og-main">
-                <div className="og-top">
-                    <div className="og-social">
-                        <a className="nav-link" href="https://github.com/openglobus/openglobus" title="GitHub">
-                            <i className="icon-github-circled" aria-hidden="true"></i>
-                        </a>
-                        <a className="nav-link" href="https://twitter.com/openglobus" title="Twitter">
-                            <i className="icon-twitter" aria-hidden="true"></i>
-                        </a>
-                        <a className="nav-link" href="https://www.linkedin.com/company/openglobus/" title="LinkedIn">
-                            <i className="icon-linkedin" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </div>
-
                 <div className="og-center">
-                    <div className="og-description">
-                        &emsp;Openglobus is an open-source javascript library for creating interactive 3D maps,
-                        with real-time
-                        surroundings rendering. It supports various high-resolution terrain providers, and imagery
-                        layers,
-                        renders thousands of 3D objects, provides geometry measurement tools, and more.
-                    </div>
-                </div>
 
-                <div className="og-subscribe">
-                    Stay tuned, and subscribe!
+                    <div className="og-top">
+                        <div className="og-social">
+                            <a className="nav-link" href="https://github.com/openglobus/openglobus" title="GitHub">
+                                <i className="icon-github-circled" aria-hidden="true"></i>
+                            </a>
+                            <a className="nav-link" href="https://twitter.com/openglobus" title="Twitter">
+                                <i className="icon-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a className="nav-link" href="https://www.linkedin.com/company/openglobus/"
+                               title="LinkedIn">
+                                <i className="icon-linkedin" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="og-description">
+                        &emsp;&emsp;Openglobus is an open-source javascript/typescript library for creating interactive
+                        high
+                        precised 3D
+                        maps, with real-time surroundings rendering. It supports various high-resolution terrain data
+                        providers, and imagery
+                        layers, renders thousands of 3D objects, provides geometry measurement tools, and more.
+                        <br/>
+                        <br/>
+                        &emsp;&emsp;Openglobus is released under the
+                        <a className="og-mit" href="https://github.com/openglobus/openglobus/blob/master/LICENSE.md">MIT
+                            License</a> and is
+                        free for both commercial and non-commercial use.
+                    </div>
+
+                    <div className="og-subscribe">
+                        Stay tuned, and subscribe!
+                    </div>
+
                 </div>
             </div>
 
             <div className="og-bottom">
-                <p className="og-copyright">© Michael Gevlich 2023. <a
-                    href="https://github.com/openglobus/openglobus/blob/master/LICENSE.md">License MIT</a></p>
+                <p className="og-copyright">© Michael Gevlich 2023</p>
             </div>
 
         </div>
